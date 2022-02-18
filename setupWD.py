5211eab83abe00436a9f96715c41d5d5ed8d70d8
ArthurVM <ArthurVM@github.com>
2022-02-18 13:38:00 +0000

    setup script

---
+++ setupWD.py
@@ -0,0 +1,166 @@
+"""
+junky and primitive install module for Lodestone_modtest
+"""
+
+from os import kill, path
+import sys
+import argparse
+from signal import alarm, signal, SIGALRM, SIGKILL
+from subprocess import PIPE, Popen
+import shutil
+import re
+import time
+
+## begin overengineered command executing class structure ##
+
+class Error(Exception):
+    """ Base class for exceptions in this module.
+    """
+    pass
+
+class InputError(Error):
+    """ Exception raised for errors in the input.
+
+    Attributes:
+        expression -- input expression in which the error occurred
+        message -- explanation of the error
+    """
+
+    def __init__(self, expression, message):
+        self.expression = expression
+        self.message = message
+
+        sys.stderr.write(f"INPUT ERROR : {self.expression}\n{self.message}")
+        sys.exit(1)
+
+class command():
+    """ A class for handling shell executed commands
+    TODO: clean up this class
+    """
+    def __init__(self, command):
+        self.command = command
+        print(f"COMMAND={self.command}")
+
+    def run(self, timeout = -1):
+        kill_tree = True
+
+        class Alarm(Exception):
+            pass
+
+        def alarm_handler(signum, frame):
+            raise Alarm
+
+        p=Popen(self.command, shell = True, stdout = PIPE, stderr = PIPE, env = None)
+
+        if timeout != -1:
+            signal(SIGALRM, alarm_handler)
+            alarm(timeout)
+
+        try:
+            stdout, stderr = p.communicate()
+            if timeout != -1:
+                alarm(0)
+
+        except Alarm as a:
+            pids = [p.pid]
+            if kill_tree:
+                pids.extend(self.get_process_children(p.pid))
+            for pid in pids:
+                # This is to avoid OSError: no such process in case process dies before getting to this line
+                try:
+                    kill(pid, SIGKILL)
+                except OSError:
+                    pass
+            return -1, '', ''
+
+        return p.returncode, stdout, stderr
+
+    def run_comm(self, if_out_return, exit=True):
+        """ Run the command with or without an exit code
+        """
+        returncode, stdout, stderr=self.run(360000)
+
+        if returncode and stderr:
+            sys.stderr.write(f"\nERROR: {self.command} FAILED!!! \n\nSTDERR: {stderr}\nSTDOUT: {stdout}\n")
+            if exit:
+                sys.exit(1)
+
+        if if_out_return:
+            return stdout
+
+    def get_process_children(self, pid):
+        p = Popen(f"ps --no-headers -o pid --ppid {pid}", shell = True,
+	         stdout = PIPE, stderr = PIPE)
+        stdout, stderr = p.communicate()
+        return [int(p) for p in stdout.split()]
+
+## end overengineered command executing class structure ##
+
+def setupWD(args):
+    """
+    """
+
+    gitline = "git clone https://github.com/Pathogen-Genomics-Cymru/tb-pipeline.git"
+    command(gitline).run_comm(0)
+
+    if not args.krakenDB:
+        getline = "wget https://genome-idx.s3.amazonaws.com/kraken/k2_standard_16gb_20200919.tar.gz"
+    else:
+        lnline = f"ln -s {args.krakenDB} ./"
+        command(lnline).run_comm(0)
+
+    if not args.bowtieIndex:
+        getline = "wget ftp://ftp.ccb.jhu.edu/pub/data/bowtie2_indexes/hg19_1kgmaj_bt2.zip"
+    else:
+        lnline = f"ln -s {args.bowtieIndex} ./"
+        command(lnline).run_comm(0)
+
+    datalnline = f"ln -s {args.testdata}"
+    command(datalnline).run_comm(0)
+
+    if args.buildSingularityimg:
+        bd = os.getcwd()
+        os.chdir("./tb-pipeline/singularity/")
+        buildline = "bash singularity_build.sh"
+        command(buildline).run_comm(0)
+        os.chdir(bd)
+
+    print("INSTALLATION FINISHED. Could have gone well, could have been catastrophic. Good luck.")
+
+def isDir(dirpath):
+    """ checks directory exists
+    """
+    if not os.path.isdir(dirpath):
+
+        msg = "{0} is not a directory".format(dirpath)
+        raise argparse.ArgumentTypeError(msg)
+
+    else:
+        return os.path.abspath(os.path.realpath(os.path.expanduser(dirpath)))
+
+def parse_args(argv):
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('script_path', action='store', help=argparse.SUPPRESS)
+    parser.add_argument('testdata', action='store', type=isDir,
+     help='Path to a directory containing test data.')
+
+    parser.add_argument('-b', '--bowtieIndex', type=isDir, action='store', default=False,
+     help='Path to the bowtie index if stored locally.')
+    parser.add_argument('-k', '--krakenDB', type=isDir, action='store', default=False,
+     help='Path to the kraken2 database if stored locally.')
+    parser.add_argument('-s', '--buildSingularityimg', action='store_true', default=False,
+     help='Build the Lodestone Singularity images. Default=False.')
+
+    args = parser.parse_args(argv)
+
+    return args
+
+def main(argv):
+    args = parse_args(argv)
+
+    setupWD(args)
+
+if __name__=="__main__":
+    main(sys.argv)
